88c88edc704df148aaa7e52687dd2f7ea4d9739d
maria <mpcarrero1@gmail.com>
2019-04-15 18:20:27 -0500

    ListView now shows images.

--- app/src/main/java/com/example/datastorage/Adapters/UsersListAdapter.kt
+++ app/src/main/java/com/example/datastorage/Adapters/UsersListAdapter.kt
@@ -2,10 +2,12 @@ package com.example.datastorage.Adapters
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.datastorage.R
 import com.example.datastorage.Modelos.User
@@ -39,6 +41,15 @@ class UsersListAdapter(private val activity: Activity, usersList: List<User>?) :
         vi = inflater.inflate(R.layout.row_item, null)
         val nombre = vi.findViewById<TextView>(R.id.Nombre)
         val edad = vi.findViewById<TextView>(R.id.Edad)
+
+
+        val img = usersList[i].image
+        if (img != null) {
+            vi.findViewById<ImageView>(R.id.userImage).setImageBitmap(
+                BitmapFactory.decodeByteArray(img, 0, img.size)
+            )
+        }
+
         nombre.text = usersList[i].name
         edad.text = usersList[i].age.toString()
         return vi
