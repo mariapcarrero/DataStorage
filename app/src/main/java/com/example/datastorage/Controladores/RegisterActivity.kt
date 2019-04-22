07f0120e259649cfe8164bf4b18b51cfecf3ff12
sdk android <android@iMac-de-admin.local>
2019-04-22 15:42:46 -0500

    Register activity code has been improved.

--- app/src/main/java/com/example/datastorage/Controladores/RegisterActivity.kt
+++ app/src/main/java/com/example/datastorage/Controladores/RegisterActivity.kt
@@ -1,99 +1,53 @@
 package com.example.datastorage.Controladores
 
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.provider.MediaStore
 import android.widget.TextView
-import android.widget.ImageView
-import android.widget.Button
 import android.widget.Toast
 import com.example.datastorage.R
 import com.example.datastorage.Servicios.LoginServices
 import com.example.datastorage.Modelos.User
-import com.karumi.dexter.Dexter
 import android.Manifest
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-import java.io.*
-import android.graphics.Bitmap
+import com.example.datastorage.Servicios.GalleryServiceForRegister
+import com.example.datastorage.Servicios.PermissionBuilder
+import com.example.datastorage.Servicios.PermissionService
 
 class RegisterActivity : AppCompatActivity() {
 
+    companion object {
+        const val USER_ALREADY_EXISTS = "El usuario ya existe en el sistema."
+        const val USER_REGISTERED_SUCCESSFULLY = "Se creo exitosamente el usuario."
+    }
+
     private lateinit var loginServices : LoginServices
+    private lateinit var permissionServices: PermissionService
+    private lateinit var galleryServiceForRegister: GalleryServiceForRegister
+
     private lateinit var imgArr : ByteArray
-    private val GALLERY : Int = 1889
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         loginServices= LoginServices(this)
 
-        requestMultiplePermisions()
-    }
+        permissionServices = PermissionService(this, PermissionBuilder().addPermission(Manifest.permission.READ_EXTERNAL_STORAGE))
+        permissionServices.requestPermissions(this)
 
-    fun requestMultiplePermisions() {
-        val context = this
-        Dexter.withActivity(this).
-            withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
-            .withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    if (report != null) {
-                        if (report.areAllPermissionsGranted()) {
-                            Toast.makeText(context, "Ya se aceptaron todos los permisos!",  Toast.LENGTH_SHORT).show()
-                        } else {
-                            Toast.makeText(context, "Se nego alguno de los permisos!",  Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    permissions: MutableList<PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-                    token?.continuePermissionRequest()
-                }
-            }).onSameThread().check()
+        galleryServiceForRegister = GalleryServiceForRegister()
     }
 
     fun uploadPhoto(view : View?) {
-        val dialog = AlertDialog.Builder(this)
-        dialog.setTitle("Select action")
-        dialog.setItems(arrayOf("Select photo from gallery"), DialogInterface.OnClickListener { dialog, which ->
-            if (which == 0) {
-                chosePhotoFromGallery()
-            }
-        })
-        dialog.show()
-    }
-
-    fun chosePhotoFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(intent, GALLERY)
+        galleryServiceForRegister.open(this)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == RESULT_CANCELED) {
+            galleryServiceForRegister.onActivityCanceled(this, requestCode, resultCode, data)
             return
         }
-
-        if (requestCode == GALLERY) {
-            if (data != null) {
-                val uri = data.data
-
-                var bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
-                val stream = ByteArrayOutputStream()
-                bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
-                imgArr = stream.toByteArray()
-
-                findViewById<ImageView>(R.id.imageView).setImageBitmap(bitmap)
-            }
-        }
+        galleryServiceForRegister.onActivityAccepted(this, requestCode, resultCode, data)
     }
 
     fun createProfile(view : View) {
@@ -105,19 +59,18 @@ class RegisterActivity : AppCompatActivity() {
         val user = User(null, name, email, age, password, imgArr)
         val exists = this.loginServices.existsUser(user)
         if (exists) {
-            Toast.makeText(this, "El usuario ya existe.",  Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, USER_ALREADY_EXISTS,  Toast.LENGTH_SHORT).show()
         } else {
             loginServices.saveUser(user, this, true)
-            Toast.makeText(this, "Se creo exitosamente el usuario.",  Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, USER_REGISTERED_SUCCESSFULLY,  Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
 
-    fun acceder(view: View)
-    {
-        val email = findViewById<TextView>(R.id.email);
-        val password = findViewById<TextView>(R.id.password);
+    fun setImageArray(array : ByteArray) {
+        imgArr = array
     }
+
 }
